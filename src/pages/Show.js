e6509a145db44d6ed1234a5ea5b33c3db9bc924f
August Felso <augustfelso@gmail.com>
2021-08-25 15:44:14 -0400

    Added to public repository

---
+++ src/pages/Show.js
@@ -0,0 +1,58 @@
+/* eslint-disable no-underscore-dangle */
+
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useShow } from '../misc/custom-hooks';
+import ShowMainData from '../components/show/ShowMainData';
+import Seasons from '../components/show/Seasons';
+import Cast from '../components/show/Cast';
+import Details from '../components/show/Details';
+
+import { ShowPageWrapper, InfoBlock } from './Show.styled';
+
+const Show = () => {
+  const { id } = useParams();
+
+  const { show, isLoading, error } = useShow(id);
+
+  if (isLoading) {
+    return <div>Data is being loaded</div>;
+  }
+
+  if (error) {
+    return <div>Error occured: {error}</div>;
+  }
+
+  return (
+    <ShowPageWrapper>
+      <ShowMainData
+        image={show.image}
+        name={show.name}
+        rating={show.rating}
+        summary={show.summary}
+        tags={show.genres}
+      />
+
+      <InfoBlock>
+        <h2>Details</h2>
+        <Details
+          status={show.status}
+          network={show.network}
+          premiered={show.premiered}
+        />
+      </InfoBlock>
+
+      <InfoBlock>
+        <h2>Seasons</h2>
+        <Seasons seasons={show._embedded.seasons} />
+      </InfoBlock>
+
+      <InfoBlock>
+        <h2>Cast</h2>
+        <Cast cast={show._embedded.cast} />
+      </InfoBlock>
+    </ShowPageWrapper>
+  );
+};
+
+export default Show;
